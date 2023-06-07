df304098d0311297a9b2b08da64e47d83b208e0e
Raaed Serag <raaedserag@gmail.com>
2023-06-07 21:54:09 +0300

    Document APIs using swagger

--- src/routes/air-quality-routes.js
+++ src/routes/air-quality-routes.js
@@ -9,6 +9,60 @@ const routerLogger = new ApiLogger("air-quality")
 
 router.use(routerLogger.requestsLoggerMiddleware)
 
+/**
+ *  @swagger
+ *  /air-quality/by-location:
+ *      get:
+ *          summary: Get Nearest Station Air Quality
+ *          description: Get Nearest Station Air Quality
+ *          tags: [Air Quality]
+ *          parameters:
+ *              - in: query
+ *                name: latitude
+ *                schema:
+ *                  type: number
+ *                required: true
+ *                description: Location Latitude
+ *              - in: query
+ *                name: longitude
+ *                schema:
+ *                  type: number
+ *                required: true
+ *                description: Location longitude
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  result:
+ *                                      type: object
+ *                                      properties:
+ *                                          pollution:
+ *                                              type: object
+ *                                              properties:
+ *                                                  ts:
+ *                                                      type: string
+ *                                                      description: Timestamp
+ *                                                  aqius:
+ *                                                      type: number
+ *                                                      description: Air Quality Index US
+ *                                                  mainus:
+ *                                                      type: string
+ *                                                      description: Main Pollutant US
+ *                                                  aqicn:
+ *                                                      type: number
+ *                                                      description: Air Quality Index China
+ *                                                  maincn:
+ *                                                      type: string
+ *                                                      description: Main Pollutant China
+ *              400:
+ *                  description: Bad Request
+ *              500:
+ *                  description: Internal Server Error
+ */
 router.get("/by-location",
     new RequestValidatorMiddleware([
         new JoiRequestQueryValidator(
@@ -20,6 +74,28 @@ router.get("/by-location",
     airQualityControllers.getNearestStationAirQuality
 );
 
+/**
+ *  @swagger
+ *  /air-quality/max-pollution:
+ *      get:
+ *          summary: Get Max Pollution
+ *          description: Get Max Pollution
+ *          tags: [Air Quality]
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  maxPolltionValue:
+ *                                      type: number
+ *                                      description: Max Pollution Value
+ *                                  maxPollutionDateTime:
+ *                                      type: string
+ *                                      description: Max Pollution Date Time
+ */
 router.get("/max-pollution", airQualityControllers.getMaxPollution)
 
 router.use(routerLogger.errorsLoggerMiddleware)
--- src/routes/express-api.js
+++ src/routes/express-api.js
@@ -1,7 +1,13 @@
 // Import Modules
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import fs from "fs";
 import express from "express";
 import "express-async-errors"
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import { errorMiddleware } from "../middlewares/index.js"
 
 import airQualityRoutes from "./air-quality-routes.js";
@@ -14,6 +20,9 @@ export function initializeExpressAPI() {
   app.use(express.urlencoded({ extended: true, limit: "10mb" }));
   app.use(cors());
 
+
+  initializeSwaggerJsdoc(app)
+
   // Apply Routes
   app.use("/air-quality", airQualityRoutes);
 
@@ -22,4 +31,41 @@ export function initializeExpressAPI() {
 
 
   return app
+}
+
+
+
+function initializeSwaggerJsdoc(app) {
+  const swaggerSpec = swaggerJsdoc({
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Air Quality API',
+        description: 'Air Quality API Docs',
+        contact: {
+          name: 'Raaed Serag',
+          email: 'raaedserag@gmail.com'
+        },
+      },
+      servers: [
+        {
+          url: `/`,
+        }
+      ],
+
+    },
+    apis: getDirectoryNestedFiles(__dirname),
+  })
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customSiteTitle: "Air Quality API"
+  }))
+}
+const getDirectoryNestedFiles = (dirPath) => {
+  return fs.readdirSync(dirPath, {
+    withFileTypes: true
+  }).reduce((files, file) => {
+    const name = path.join(dirPath, file.name);
+    const isDirectory = file.isDirectory();
+    return isDirectory ? [...files, ...getDirectoryNestedFiles(name)] : [...files, name];
+  }, []);
 }
